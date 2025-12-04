1942ebba19c7167256be732b61c2097831b1a2b4
caang2023119isinf0020-dot <caang.2023119isinf0020@aluno.ifpi.edu.br>
2025-12-03 22:14:07 -0300

    Create 5° Questão.cpp

---
+++ 5° Questão.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int vetor[5];
+    int soma = 0;
+    float media;
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Digite o valor " << i + 1 << ": ";
+        cin >> vetor[i];
+        soma += vetor[i]; 
+    }
+
+    media = soma / 5.0;
+
+    cout << "Soma total: " << soma << endl;
+    cout << "Media aritmetica: " << media << endl;
+
+    return 0;
+}
